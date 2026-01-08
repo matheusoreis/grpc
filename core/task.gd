646797e2c64f055fe3009864e13746dbd90b38ea
Fabio Smuu <fabiosmuu@gmail.com>
2026-01-08 14:03:48 -0300

    Iniciando doc e otimizando algumas funções

--- core/task.gd
+++ core/task.gd
@@ -1,5 +1,9 @@
-extends Node
+extends RefCounted
+## Representa uma tarefa assíncrona de RPC.
+##
+## Utilizada internamente para gerenciar o retorno de chamadas INVOKE.
 class_name GRpcTask
 
+## Emitido quando a tarefa é concluída e o resultado é recebido.
 @warning_ignore("unused_signal")
 signal done(value: Variant)
