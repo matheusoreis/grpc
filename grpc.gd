39e8ee827f3fb94b4cfb2df19e5899b23164da54
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-01-09 08:08:18 -0300

    Remoção do autoload não mais necessário

--- grpc.gd
+++ grpc.gd
@@ -1,14 +1,2 @@
 @tool
 extends EditorPlugin
-## Plugin principal para o addon gRPC.
-##
-## Gerencia a ativação e desativação do singleton de rede no editor.
-
-## Chamado quando o plugin é ativado. Adiciona o singleton 'Network'.
-func _enable_plugin() -> void:
-	var network_path: String = "res://addons/grpc/network/network.gd"
-	self.add_autoload_singleton("Network", network_path)
-
-## Chamado quando o plugin é desativado. Remove o singleton 'Network'.
-func _disable_plugin() -> void:
-	self.remove_autoload_singleton("Network")
